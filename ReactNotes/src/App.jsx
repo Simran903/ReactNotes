f583edc20bcc6dde531a64294cdfcff330d6568e
Simran <itssimran903@gmail.com>
2024-01-17 11:03:19 +0530

    commit

--- ReactNotes/src/App.jsx
+++ ReactNotes/src/App.jsx
@@ -4,7 +4,7 @@ import { constNotes } from "./constants";
 import { nanoid } from "nanoid";
 
 const App = () => {
-  // const [notes, setNotes] = useState([constNotes]);
+  const [notes, setNotes] = useState(constNotes);
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -12,13 +12,13 @@ const App = () => {
       text: text,
       date: date.toLocaleDateString(),
     };
-    const newNotes = [...constNotes, newNote];
-    // setNotes(newNotes);
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
   };
 
   return (
     <div className="container">
-      <NotesList notes={constNotes} handleAddNote={addNote} />
+      <NotesList notes={notes} handleAddNote={addNote} />
     </div>
   );
 };
